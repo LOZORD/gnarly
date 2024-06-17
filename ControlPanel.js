44fe3636700157193040e08f4fd28f4c14faf2bf
Leo Rudberg <ljr@google.com>
2024-06-17 16:09:31 -0400

    Add ControlPanel class for organizing sliders.

---
+++ ControlPanel.js
@@ -0,0 +1,25 @@
+class ControlPanel {
+    #controls;
+
+    constructor(controlObjects, x = 20, y = 20, yOffset = 25) {
+        this.#controls = new Map();
+        for (let obj of controlObjects) {
+            const {name, label, min, max, step, value } = obj;
+            const slider = new LabelledSlider(label, x, y, min, max, value, step);
+            y += yOffset;
+            this.#controls.set(name, slider);
+        }
+    }
+
+    draw() {
+        this.#controls.forEach(value => value.draw());
+    }
+
+    values() {
+        const ret = new Map();
+        for (let [k, v] of this.#controls.entries()) {
+            ret.set(k, v.value());
+        }
+        return ret;
+    }
+}
