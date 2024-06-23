b01a35d87aad09070369860fe96360d614a2de35
Leo Rudberg <ljr@google.com>
2024-06-22 23:30:51 -0400

    Display frame rate in control panel (currently disabled).

--- ControlPanel.js
+++ ControlPanel.js
@@ -1,12 +1,19 @@
 'use strict';
 
 class ControlPanel {
+    #SHOW_FRAME_RATE = false;
+
+    #frameRateSpan;
     #controls;
 
     constructor(controlObjects, x = 20, y = 20, yOffset = 25) {
+        this.#frameRateSpan = createSpan();
+        this.#frameRateSpan.style('background-color', 'indigo');
+        this.#frameRateSpan.style('color', 'white');
+        this.#frameRateSpan.style('font-family', 'monospace');
         this.#controls = new Map();
         for (let obj of controlObjects.sort((a, b) => a.name.localeCompare(b.name))) {
-            const {name, label, min, max, step, value, disabled } = obj;
+            const { name, label, min, max, step, value, disabled } = obj;
             const slider = new LabelledSlider(label, x, y, min, max, value, step, disabled || false);
             y += yOffset;
             this.#controls.set(name, slider);
@@ -14,6 +21,9 @@ class ControlPanel {
     }
 
     draw() {
+        if (this.#SHOW_FRAME_RATE) {
+            this.#frameRateSpan.html(`Frame Rate: ${frameRate()} fps`);
+        }
         this.#controls.forEach(value => value.draw());
     }
 
